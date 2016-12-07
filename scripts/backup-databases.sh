92fe775a54cabaeef521f27d6c023732c6835bdf
anthonykgross <anthony.k.gross@gmail.com>
2016-12-07 15:03:38 +1100

    Update backup-databases.sh

--- scripts/backup-databases.sh
+++ scripts/backup-databases.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 date=`date "+%Y-%m-%d_%H:%M:%S"`
+source /root/.bash_profile
 /usr/local/mysql/bin/mysqldump --verbose --user=root --password=$MYSQL_ROOT_PASSWORD --all-databases > /src/$date.sql
